4d5176d04c462201c1e69393fd3cd3649578376d
bvoo <60059541+bvoo@users.noreply.github.com>
2022-04-30 17:32:39 -0700

    opengraph agane

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
     export let params; 
 
-    console.log(params)
-
     let copyIcon = 'fal fa-copy';
     
-    let authorText = "";
     let titleText = "";
+    let siteName = "";
     let descriptionText = "";
     let sidebarColor = "";
     let imageUrl = "";
-    let redirectUrl = "";
     let url = "";
+
     
     async function generate() {
         let embed_url = "";
 
-        if (authorText != "") {
-            authorText = encodeURIComponent(authorText);
-            embed_url += "?author=" + authorText;
-        }
         if (titleText != "") {
             titleText = encodeURIComponent(titleText);
             embed_url += "&title=" + titleText;
+        }
+        if (siteName != "") {
+            siteName = encodeURIComponent(siteName);
+            embed_url += "&sitename=" + siteName;
         }
         if (descriptionText != "") {
             descriptionText = encodeURIComponent(descriptionText);
@@ -35,10 +33,6 @@
         if (imageUrl != "") {
             imageUrl = encodeURIComponent(imageUrl);
             embed_url += "&imageUrl=" + imageUrl;
-        }
-        if (redirectUrl != "") {
-            redirectUrl = encodeURIComponent(redirectUrl);
-            embed_url += "&redirectUrl=" + redirectUrl;
         }
 
         embed_url = "https://embed.bvoo.xyz/" + embed_url;
@@ -73,23 +67,19 @@ export async function load({ url }) {
 
 <svelte:head>
     <title>{params.title}</title>
+
     <meta content='en_US' name='locale' />
-    <meta content='{params.sidebarColor}' name='theme-color' />
-    <meta content='{params.description}' name='description' />
-    <meta content='{params.description}' property='og:description' />
-    <meta content='{params.description}' name='twitter:description' />
+    <meta content='website' property='og:type' />
 
     <meta content='{params.title}' property='og:title' />
-    <meta content='{params.title}' name='twitter:title' />
-    <meta content='{params.title}' property='og:site_name' />
+    <meta content='{params.siteName}' property='og:site_name' />
+    
+    <meta content='{params.description}' property='og:description' />
+    <meta content='{params.description}' name='twitter:description' />
+    
+    <meta content='{params.sidebarColor}' name='theme-color' />
+    
     <meta content='{params.imageUrl}' name='og:image' />
-
-    <meta content='website' property='og:type' />
-    <meta content='{params.redirectUrl}' property='og:url' />
-    <meta content='{params.imageUrl}' name='twitter:image' />
-    <meta content='{params.imageUrl}' name='twitter:card' />
-    <meta content='{params.author}' name='twitter:creator' />
-    <meta content='{params.author}' name='twitter:site' />
 </svelte:head>
 
 <div class="h-[4em]" />
@@ -99,12 +89,11 @@ export async function load({ url }) {
     <div class="h-2" />
     <div class="text-2xl flex flex-col space-y-6 sm:gap-0 w-full">
       <div class="w-32" />
-      <input type="text" bind:value={ authorText } placeholder="Author" />
       <input type="text" bind:value={ titleText } placeholder="Title" />
+      <input type="text" bind:value={ siteName } placeholder="Site Name" />
       <input type="text" bind:value={ descriptionText } placeholder="Description" />
       <input type="text" bind:value={ sidebarColor } placeholder="Sidebar Color (#FFFFFF)" />
       <input type="text" bind:value={ imageUrl } placeholder="Image URL" />
-      <input type="text" bind:value={ redirectUrl } placeholder="Redirect URL" />
       <span class="font-mono text-secondary">
         <button id="generate" on:click={() => generate()}> Generate </button>
       </span>
